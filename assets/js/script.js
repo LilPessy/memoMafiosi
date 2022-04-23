3cfd5bf69aac6bb4e7f8e7da7a10f4071b298d9b
LilPessy <pettidaniele1f@gmail.com>
2022-04-23 12:36:09 +0200

    creato scritpt
    permette di flippare le carte e generare i nomi delle carte nei data-attributes

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let cards = document.querySelectorAll('.card');
+
+for(let i = 0; i < cards.length; i++){
+    cards[i].addEventListener('click', function(e){
+        let card = e.currentTarget;
+        card.classList.toggle('selected');
+        let selectedCards = document.querySelectorAll('.card.selected');
+        console.log(selectedCards);
+
+        if(selectedCards.length>=2){
+            for(let i = 0; i < selectedCards.length; i++){
+                setTimeout(function(){
+                    selectedCards[i].classList.remove('selected');
+                }, 1000);
+            }
+        }
+    });
+}
+
+let names = ["lil", "lil", "cix", "cix", "genb", "genb", "gabbo", "gabbo", "nto", "nto"];
+
+let cardContent = []; 
+
+for(let i=0; i<names.length; i++){
+    let name = names[Math.floor(Math.random()*names.length)];
+    let j = names.indexOf(name);
+    names.splice(j,1);
+    i--;
+    cardContent.push(name);
+}
+
+console.log(cardContent);
+
+for(let i=0; i<cardContent.length; i++){
+    cards[i].dataset.name = cardContent[i];
+}
+
